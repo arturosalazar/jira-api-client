19d3e986c62e3499d8d3ddb49ef05109dfdedd8d
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2025-07-08 20:56:18 -0500

    Initial commit: Spring Boot Jira API client

---
+++ src/main/java/com/arturosalazar/jira_api_client/JiraApiClientApplication.java
@@ -0,0 +1,13 @@
+package com.arturosalazar.jira_api_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraApiClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraApiClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arturosalazar/jira_api_client/JiraApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturosalazar.jira_api_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraApiClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
