2003f4cda36230b930ab3a50634ec2b0aed304fb
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2025-07-09 15:26:47 -0500

    chore: wire JiraIssueService into main application for testing

--- src/main/java/com/arturosalazar/jira_api_client/JiraApiClientApplication.java
+++ src/main/java/com/arturosalazar/jira_api_client/JiraApiClientApplication.java
@@ -1,13 +1,28 @@
 package com.arturosalazar.jira_api_client;
 
+import com.arturosalazar.jira_api_client.config.ProjectConfig;
+import com.arturosalazar.jira_api_client.services.JiraIssueService;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class JiraApiClientApplication {
 
+	private JiraIssueService jiraIssueService;
+
+	public JiraApiClientApplication(JiraIssueService jiraIssueService){
+		this.jiraIssueService = jiraIssueService;
+	}
+
 	public static void main(String[] args) {
+		var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 		SpringApplication.run(JiraApiClientApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init(){
+		jiraIssueService.createIssue();
+	}
 }
