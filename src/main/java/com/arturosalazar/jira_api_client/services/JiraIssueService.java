e20519eca58dbf414be0d80e29c433bfc9687a6c
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2025-07-09 15:56:46 -0500

    chore: load secret config in JiraIssueService using @Value

--- src/main/java/com/arturosalazar/jira_api_client/services/JiraIssueService.java
+++ src/main/java/com/arturosalazar/jira_api_client/services/JiraIssueService.java
@@ -1,10 +1,20 @@
 package com.arturosalazar.jira_api_client.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class JiraIssueService {
+
+    @Value("${jira.domain}")
+    private String jiraDomain;
+
+    @Value("${jira.id}")
+    private String jiraId;
+
+    @Value("${jira.token}")
+    private String jiraToken;
 
     private RestTemplate restTemplate;
 
@@ -13,5 +23,8 @@ public class JiraIssueService {
 
     public void createIssue(){
         //Use Rest Template to create
+        System.out.println("Jira Domain: " + jiraDomain);
+        System.out.println("Jira ID: " + jiraId);
+        System.out.println("Jira Token: " + jiraToken);
     }
 }
