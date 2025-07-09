a56b1ba6cdc0ad6e3dc9621e88706840b98c24f8
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2025-07-09 18:03:47 -0500

    feat: implement createIssue method in JiraIssueService to create basic Jira issue

--- src/main/java/com/arturosalazar/jira_api_client/services/JiraIssueService.java
+++ src/main/java/com/arturosalazar/jira_api_client/services/JiraIssueService.java
@@ -1,8 +1,11 @@
 package com.arturosalazar.jira_api_client.services;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Base64;
 
 @Service
 public class JiraIssueService {
@@ -19,12 +22,62 @@ public class JiraIssueService {
     private RestTemplate restTemplate;
 
     public JiraIssueService(RestTemplate restTemplate){
+        this.restTemplate = restTemplate;
     }
 
     public void createIssue(){
-        //Use Rest Template to create
-        System.out.println("Jira Domain: " + jiraDomain);
-        System.out.println("Jira ID: " + jiraId);
-        System.out.println("Jira Token: " + jiraToken);
+        // Request headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        String authString = jiraId + ":" + jiraToken;
+        // Convert auth string to Base 64 per Jira API Basic authentication requirements
+        String encodedAuthString = Base64.getEncoder().encodeToString(authString.getBytes());
+        headers.set("Authorization", "Basic " + encodedAuthString);
+
+        // Request body
+        String requestBody = """
+                {
+                  "fields": {
+                    "project": {
+                      "key": "SCRUM"
+                    },
+                    "summary": "Example Task Summary",
+                    "description": {
+                      "type": "doc",
+                      "version": 1,
+                      "content": [
+                        {
+                          "type": "paragraph",
+                          "content": [
+                            {
+                              "type": "text",
+                              "text": "This is a simple task created via the Jira API."
+                            }
+                          ]
+                        }
+                      ]
+                    },
+                    "issuetype": {
+                      "name": "Task"
+                    }
+                  }
+                }
+                
+                """;
+
+        HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
+
+        // Request domain
+        String requestDomain = jiraDomain + "/rest/api/3/issue";
+        ResponseEntity<String> response = restTemplate.exchange(
+                requestDomain,          // URL
+                HttpMethod.POST,        // HTTP Method
+                requestEntity,          // Request Entity (body and headers)
+                String.class            // Expected Response Type
+        );
+
+        System.out.println(response.getBody());
+        System.out.println(response.getStatusCode());
+
     }
 }
