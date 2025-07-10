5de8fbd288d2810cb9a37aaa6b187df2accca4a3
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2025-07-10 16:51:12 -0500

    feat: update JiraIssueResponse to also parse out the 'self' field in the json response

--- src/main/java/com/arturosalazar/jira_api_client/dto/JiraIssueResponse.java
+++ src/main/java/com/arturosalazar/jira_api_client/dto/JiraIssueResponse.java
@@ -3,12 +3,14 @@ package com.arturosalazar.jira_api_client.dto;
 public class JiraIssueResponse {
     private String id;
     private String key;
+    private String self;
 
     public JiraIssueResponse() {    }
 
     public JiraIssueResponse(String id, String key) {
         this.id = id;
         this.key = key;
+        this.self = self;
     }
 
     public String getId() {
@@ -25,5 +27,13 @@ public class JiraIssueResponse {
 
     public void setKey(String key) {
         this.key = key;
+    }
+
+    public String getSelf() {
+        return self;
+    }
+
+    public void setSelf(String self) {
+        this.self = self;
     }
 }
